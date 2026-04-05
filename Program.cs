337e19602db01d9bc06ac8846a7ff88904977415
gector727 <gectorashera@gmail.com>
2026-04-05 21:30:17 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JurnalModul6_103082400004_Muh_Aqsa_Sirojudin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
